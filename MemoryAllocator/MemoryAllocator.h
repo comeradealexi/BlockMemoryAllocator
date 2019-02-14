a98cbf6afece7819cb2ab5761e4c032dc7d4d16c
comeradealexi <houghton1411@aol.com>
2019-02-14 07:17:48 +0000

    array

--- MemoryAllocator/MemoryAllocator.h
+++ MemoryAllocator/MemoryAllocator.h
@@ -13,12 +13,13 @@ namespace Templated
 {
 	struct PoolSizeConstructor
 	{
-		constexpr PoolSizeConstructor(size_t poolSize, size_t poolCount) : kPoolSize(poolSize), kPoolCount(poolCount)
+		constexpr PoolSizeConstructor(size_t poolSize, size_t poolCount) : kPoolSize(poolSize), kPoolCount(poolCount), kBlockTotalSize(poolSize * poolCount)
 		{
 
 		}
 		const size_t kPoolSize = 0;
 		const size_t kPoolCount = 0;
+		const size_t kBlockTotalSize = 0;
 	};
 	struct CPPAllocator
 	{
@@ -42,11 +43,37 @@ namespace Templated
 		//NOTE TO SELF USE SELF DEFINED ARRAYS
 		static constexpr PoolSizeConstructor kPoolSizes[] = 
 		{ 
-			{1024*1024		,32},
-			{1024 * 1024 * 2,32},
-
-
+			//Size, Count
+			{256, 1024},
+			{512, 1024},
+			{768, 1024},
+			{1024, 1024},
+			{1536, 1024},
+			{1024 * 1024,		32},
+			{1024 * 1024 * 2,	32},
+			{1024 * 1024 * 3,	32},
+			{1024 * 1024 * 4,	32},
+			{1024 * 1024 * 5,	32},
+			{1024 * 1024 * 6,	32},
+			{1024 * 1024 * 7,	32},
+			{1024 * 1024 * 8,	32},
+			{1024 * 1024 * 9,	32},
+			{1024 * 1024 * 10,	32},
+			{1024 * 1024 * 12,	16},
+			{1024 * 1024 * 24,	8},
+			{1024 * 1024 * 32,	8},
+			{1024 * 1024 * 36,	8},
+			{1024 * 1024 * 42,	8},
+			{1024 * 1024 * 48,	8},
+			{1024 * 1024 * 52,	8},
+			{1024 * 1024 * 56,	4},
+			{1024 * 1024 * 60,	2},
+			{1024 * 1024 * 64,	2},
+			{1024 * 1024 * 68,	2},
+			{1024 * 1024 * 72,	2},
 		};
+		static constexpr auto kArrayTotalSize = sizeof(kPoolSizes) / sizeof(kPoolSizes[0]);
+
 	public:
 		Memory Allocate(Size memorySize, Size memoryAlignment)
 		{
@@ -95,20 +122,21 @@ namespace Templated
 		}
 
 		template<typename T>
-		void DebugPrint(T& dbgPrint)
+		void DebugPrint(T& dbgPrint, bool bOnlyPrintActivePools)
 		{
 			dbgPrint << "Memory Allocator Info:" << "\n";
-			m_firstPool.DebugPrint(1, dbgPrint);
+			m_firstPool.DebugPrint(1, dbgPrint, bOnlyPrintActivePools);
 
 		}
 
 	private:
-		template<typename POOL_ALLOCATOR, typename T_ALLOCATOR::Size T_BLOCK_SIZE, typename T_ALLOCATOR::Size T_BLOCK_COUNT, bool T_BOOL>
+		template<typename POOL_ALLOCATOR, size_t T_ARRAY_IDX, bool T_BOOL>
 		struct PoolList
 		{
-			static constexpr auto kPoolSizeBytes = T_BLOCK_SIZE * T_BLOCK_COUNT;
-			static constexpr auto kBlockSize = T_BLOCK_SIZE;
-			static constexpr auto kBlockCount = T_BLOCK_COUNT;
+			static constexpr auto kBlockSize = POOL_ALLOCATOR::kPoolSizes[T_ARRAY_IDX].kPoolSize;
+			static constexpr auto kBlockCount = POOL_ALLOCATOR::kPoolSizes[T_ARRAY_IDX].kPoolCount;
+			static constexpr auto kPoolSizeBytes = kBlockSize * kBlockCount;
+
 			PoolList(T_ALLOCATOR& platformAllocator) : m_platformAllocator(platformAllocator), m_nextPool(platformAllocator)
 			{
 
@@ -116,7 +144,7 @@ namespace Templated
 
 			inline Memory Allocate(typename T_ALLOCATOR::Size memorySize, typename T_ALLOCATOR::Type memoryType)
 			{
-				if (memorySize <= T_BLOCK_SIZE)
+				if (memorySize <= kBlockSize)
 				{
 					Memory newMem = std::make_shared<LocalAllocation>();
 
@@ -130,7 +158,7 @@ namespace Templated
 						{
 							newMem->blockIdx = *allocation;
 							newMem->m_poolAllocatedFrom = std::static_pointer_cast<PoolBase>(pool);
-							newMem->m_platformMemory = m_platformAllocator.Offset(pool->m_platformMemory, newMem->blockIdx * T_BLOCK_SIZE);
+							newMem->m_platformMemory = m_platformAllocator.Offset(pool->m_platformMemory, newMem->blockIdx * kBlockSize);
 							return newMem;
 						}
 					}
@@ -150,36 +178,38 @@ namespace Templated
 			{
 				m_pools.push_back(std::make_shared<Pool>());
 				auto& newPool = m_pools.back();
-				newPool->m_platformMemory = m_platformAllocator.Allocate(T_BLOCK_SIZE * T_BLOCK_COUNT, POOL_ALLOCATOR::kAlignment);
+				newPool->m_platformMemory = m_platformAllocator.Allocate(kBlockSize * kBlockCount, POOL_ALLOCATOR::kAlignment);
 				return newPool;
 			}
 
 			template<typename T>
-			inline void DebugPrint(size_t poolNumber, T& dbgPrint)
+			inline void DebugPrint(size_t poolNumber, T& dbgPrint, bool bOnlyPrintActivePools)
 			{
-				dbgPrint.precision(4);
-				dbgPrint << "#" << poolNumber << "  ";
-				dbgPrint << (size_t)T_BLOCK_SIZE;
-				dbgPrint << "(" << static_cast<float>(T_BLOCK_SIZE) / 1024.0f / 1024.0f << "mb)";
-				dbgPrint << "x";
-				dbgPrint << (size_t)T_BLOCK_COUNT;
-				dbgPrint << "=" << static_cast<size_t>(T_BLOCK_SIZE * T_BLOCK_COUNT);
-				dbgPrint << "(" << static_cast<float>(T_BLOCK_SIZE * T_BLOCK_COUNT) / 1024.0f / 1024.0f << "mb)";
-				dbgPrint << "\n";
-
-				dbgPrint << "Pool Count:" << m_pools.size() << "\n";
-				m_nextPool.DebugPrint<T>(poolNumber + 1, dbgPrint);
+				if (!bOnlyPrintActivePools || (bOnlyPrintActivePools && m_pools.size() > 0))
+				{
+					dbgPrint.precision(4);
+						dbgPrint << "#" << poolNumber << "  ";
+						dbgPrint << (size_t)kBlockSize;
+						dbgPrint << "(" << static_cast<float>(kBlockSize) / 1024.0f / 1024.0f << "mb)";
+						dbgPrint << "x";
+						dbgPrint << (size_t)kBlockCount;
+						dbgPrint << "=" << static_cast<size_t>(kBlockSize * kBlockCount);
+						dbgPrint << "(" << static_cast<float>(kBlockSize * kBlockCount) / 1024.0f / 1024.0f << "mb)";
+						dbgPrint << "\n";
+						dbgPrint << "Pool Count:" << m_pools.size() << "\n";
+				}
+				m_nextPool.DebugPrint<T>(poolNumber + 1, dbgPrint, bOnlyPrintActivePools);
 			}
 
 			struct Pool : public PoolBase
 			{
 				Pool()
 				{
-					for (size_t i = 0; i < T_BLOCK_COUNT; i++)
+					for (size_t i = 0; i < kBlockCount; i++)
 						m_allocationList.push_back(i);					
 				}
 
-				std::array<typename T_ALLOCATOR::Type, T_BLOCK_COUNT> m_typeList = {};
+				std::array<typename T_ALLOCATOR::Type, kBlockCount> m_typeList = {};
 				std::list<size_t> m_allocationList = {};
 				typename T_ALLOCATOR::Memory m_platformMemory = T_ALLOCATOR::kMemoryDefault;
 
@@ -190,7 +220,7 @@ namespace Templated
 				}
 				std::optional<size_t> Allocate(typename T_ALLOCATOR::Type memoryType)
 				{
-					if (m_activeAllocationCount == T_BLOCK_COUNT)
+					if (m_activeAllocationCount == kBlockCount)
 						return {};
 
 					if (m_allocationList.size() == 0)
@@ -209,22 +239,20 @@ namespace Templated
 			std::vector<std::shared_ptr<Pool>> m_pools;
 			T_ALLOCATOR& m_platformAllocator;
 
-			static constexpr size_t kNEXT_BLOCK_SIZE = T_BLOCK_SIZE / 2;
-			static constexpr size_t kNEXT_BLOCK_COUNT = (T_BLOCK_COUNT * 2);
-			static constexpr bool kLAST_VALID_POOL = kNEXT_BLOCK_SIZE > POOL_ALLOCATOR::kMinAllocationSizeBytes;
+			static constexpr bool kLAST_VALID_POOL = (T_ARRAY_IDX + 1) < POOL_ALLOCATOR::kArrayTotalSize;
 
-			PoolList<POOL_ALLOCATOR, kNEXT_BLOCK_SIZE, kNEXT_BLOCK_COUNT, kLAST_VALID_POOL> m_nextPool;
+			PoolList<POOL_ALLOCATOR, T_ARRAY_IDX + 1, kLAST_VALID_POOL> m_nextPool;
 		};
 
-		template<typename POOL_ALLOCATOR, typename T_ALLOCATOR::Size T_BLOCK_SIZE, typename T_ALLOCATOR::Size T_BLOCK_COUNT>
-		struct PoolList<POOL_ALLOCATOR, T_BLOCK_SIZE, T_BLOCK_COUNT, false>
+		template<typename POOL_ALLOCATOR, size_t T_ARRAY_IDX>
+		struct PoolList<POOL_ALLOCATOR, T_ARRAY_IDX, false>
 		{
 			PoolList(T_ALLOCATOR& platformAllocator)
 			{
 			}
 
 			template<typename T>
-			inline void DebugPrint(size_t poolNumber, T& dbgPrint)
+			inline void DebugPrint(size_t poolNumber, T& dbgPrint, bool bOnlyPrintActivePools)
 			{
 			}
 
@@ -243,7 +271,7 @@ namespace Templated
 		//};
 		
 		T_ALLOCATOR&		m_allocator;
-		PoolList<T_ALLOCATOR, T_ALLOCATOR::kMaxAllocationSize, T_ALLOCATOR::kMaxAllocationCount, true> m_firstPool;
+		PoolList<T_ALLOCATOR, 0, true> m_firstPool;
 		std::mutex			m_mutex;
 	};
 }
--- MemoryAllocator/Source.cpp
+++ MemoryAllocator/Source.cpp
@@ -4,10 +4,12 @@ int main()
 {
 	Templated::CPPAllocator cppAllocator;
 	Templated::MemoryAllocator<Templated::CPPAllocator> memoryPools(cppAllocator);
-	memoryPools.DebugPrint(std::cout);
+	memoryPools.DebugPrint(std::cout, false);
 	{
 		auto mem1 = memoryPools.Allocate(1024, Templated::CPPAllocator::Type::Other);
-		auto mem2 = memoryPools.Allocate(1024, Templated::CPPAllocator::Type::Other);
+		auto mem2 = memoryPools.Allocate(1024 + 1, Templated::CPPAllocator::Type::Other);
+		memoryPools.DebugPrint(std::cout, true);
 	}
+	memoryPools.DebugPrint(std::cout, true);
 	return 0;
 }
